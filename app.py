c18344b02376c5fc3842aab9f71218060911ba84
Emily <emsager7@gmail.com>
2023-09-28 15:00:39 -0700

    thursday push

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from surveys import Survey
-from flask import Flask, request, render_template,  redirect, flash,  jsonify, session
-from random import randint,  choice, sample
+from surveys import satisfaction_survey as survey
+from flask import Flask, request, render_template, redirect
+# from random import randint,  choice, sample
 from flask_debugtoolbar import DebugToolbarExtension
+
+    #initialize a variable called responses to be an empty list. As people store their answers
+    
+responses = []
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "potatolife"
 debug = DebugToolbarExtension(app)
 
-RESPONSES = []
+
 
 @app.route('/')
 def index():
     "Set up home page"
-    title = Survey.title
-    instructions = Survey.instructions
-    return render_template("home.html", title=title, instructions=instructions)
+    return render_template("home.html", survey=survey)
+
+@app.route('/begin', methods=["POST"])
+def start_survey():
+    """Clear the response key"""
+
+    return redirect('/questions/0')
