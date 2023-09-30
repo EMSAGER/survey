b904e308103cbf016cfb2e0adfa6095a4a272945
Emily <emsager7@gmail.com>
2023-09-30 12:59:52 -0700

    flash update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from surveys import satisfaction_survey as survey
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 # from random import randint,  choice, sample
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -10,6 +10,7 @@ RESPONSES = []
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "potatolife"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 
@@ -22,18 +23,42 @@ def index():
 @app.route("/begin", methods=["POST"])
 def start_survey():
     """Clear the response list & begin survey """
-    responses.clear()
+    RESPONSES.clear()
     return redirect("/question/0")
 
-@app.route("/question/<int:qid>")
-def questions(qid):
-    """Show each survey question on an individual page"""
-    questions = survey.questions[qid]
-    return render_template("question.html", question_num=qid, question=questions)
 
 @app.route("/answer", methods=["POST"])
 def record_answer():
     """Store each question's answer in the RESPONSE list"""
-    answer = request.form['answer']
-    RESPONSES.add(answer)
-    return render_template("/question/")
+    choice = request.form['answer']
+    RESPONSES.append(choice)
+        #two options for the survey: either its completed or it needs to load the next question. 
+    if (len(RESPONSES) == len(survey.questions)):
+            #survey is complete
+        return render_template("/complete.html")
+    else:
+        #load the next survey question
+            #how to get the next questions based on location
+            #use the length of responses to determine the question number & f statements
+        return redirect(f"/question/{len(RESPONSES)}")
+    
+@app.route("/question/<int:qid>")
+def questions(qid):
+    """Show each survey question on an individual page"""
+    if RESPONSES is None:
+        return render_template("/")
+
+    if (len(RESPONSES) == len(survey.questions)):
+            #survey is complete
+        return render_template("/complete.html")
+    
+    if(len(RESPONSES)) != qid:
+        flash(f"Invalid question id: {qid}.", 'error')
+        return redirect(f"/question/{len(RESPONSES)}")
+  
+    questions = survey.questions[qid]
+    return render_template("question.html", question_num=qid, question=questions)
+
+@app.route("/complete")
+def completed_survey():
+    return render_template("complete.html")
