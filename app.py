c29a588fe115fe4e5c32b9f4c03957da125556f4
Emily <emsager7@gmail.com>
2023-09-30 11:16:36 -0700

    saturday am push

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 
     #initialize a variable called responses to be an empty list. As people store their answers
-    
+
 responses = []
 
 app = Flask(__name__)
@@ -21,6 +21,12 @@ def index():
 
 @app.route('/begin', methods=["POST"])
 def start_survey():
-    """Clear the response key"""
+    """Clear the response list & begin survey """
+    responses.clear()
+    return redirect('/question/0')
 
-    return redirect('/questions/0')
+@app.route('/question/<int:qid>')
+def questions(qid):
+    """Show each survey question on an individual page"""
+    question = survey.question[qid]
+    return render_template('question.html', question_num=qid, question=question, survey=survey)
